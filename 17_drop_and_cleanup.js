958b15c5e0ce2e3dbd776fa9b767928f7e21ec03
Edward Hunt <57264981+ed7hunt@users.noreply.github.com>
2020-02-24 08:41:22 -0500

    Create 17_drop_and_cleanup.js

---
+++ 17_drop_and_cleanup.js
@@ -0,0 +1,8 @@
+function drop() {
+  svg.append('circle').attr('r', 15).attr('fill', pickRandom(color)).attr('cx', pickRandom(slot)).attr('cy', 0).transition().duration(2000).ease(d3.easeBounceOut).attr('cy', 165);
+};
+function cleanUp() {
+  d3.selectAll('circle').transition().duration(1000).attr('cx', 500).ease(d3.easeCubicIn).remove();
+};
+redButton.on('click', drop);
+blueButton.on('click', cleanUp);
